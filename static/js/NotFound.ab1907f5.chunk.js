883d4f9e2c845be5d4336e81d56940f9f723e365
SerhiiPodaiko <37670910+SerhiiPodaiko@users.noreply.github.com>
2022-06-15 21:46:34 +0300

    Updates

---
+++ static/js/NotFound.ab1907f5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpizza=self.webpackChunkpizza||[]).push([[187],{4949:function(u,e,n){n.r(e);n(2791);var a=n(2239),r=n(184);e.default=function(){return(0,r.jsx)(a.vr,{})}}}]);
+//# sourceMappingURL=NotFound.ab1907f5.chunk.js.map
